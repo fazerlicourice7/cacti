1c40305ff476cb6bb3f7170f5e4409108eb63d7b
dwoen <amwoen@stanford.edu>
2024-11-13 23:32:24 -0800

    wire initialization

--- cacti_python/wire.py
+++ cacti_python/wire.py
@@ -46,10 +46,12 @@ class Wire(Component):
         self.wire_spacing = 0
         self.repeater_size = 0 
 
+        org_wire_length = self.wire_length
         if Wire.initialized != 1:
             # print("Initializing Wire")
             self.__init_wire_simple(w_s, s_s, wp, resistivity, dt)
 
+        self.wire_length = org_wire_length
         self.calculate_wire_stats()
 
         self.repeater_spacing *= 1e6
